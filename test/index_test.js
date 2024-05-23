060bd7954274a485d2770ab2076406e38623b33d
aldair diaz <irving.s21diaz@gmail.com>
2024-05-23 10:46:22 -0600

    Refactor part 1

--- test/index_test.js
+++ test/index_test.js
@@ -4,13 +4,15 @@ const Calculate = require("../index.js");
 describe("Calculate", () => {
     describe(".factorial", () => {
         it("returns the factorial of n", () => {
-
             // Setup
-            
+            const expected = 120;
+            const input = 5;
+
             // exercise
+            const result = Calculate.factorial(input);
 
             // verification
-            assert.strictEqual(Calculate.factorial(5), 120);
+            assert.strictEqual(result, expected);
         })
 
     })

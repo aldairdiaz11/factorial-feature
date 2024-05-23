afc90867d14512ecceb80a14c06d022295b660ec
aldair diaz <irving.s21diaz@gmail.com>
2024-05-23 10:41:00 -0600

    Get into the red part 1a

--- test/index_test.js
+++ test/index_test.js
@@ -3,6 +3,15 @@ const Calculate = require("../index.js");
 
 describe("Calculate", () => {
     describe(".factorial", () => {
-        
+        it("returns the factorial of n", () => {
+
+            // Setup
+            
+            // exercise
+
+            // verification
+            assert.strictEqual(Calculate.factorial(5), 125);
+        })
+
     })
 })
